7e99cca24e41043329d5ece686a1874a5c8ddf23
jacopo-derrico <derricoj.design@gmail.com>
2024-02-27 18:26:51 +0100

    moved function to functions.php

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+    function generatePassword($charLength) {
+        $lowercase = "abcdefgijklmnopqrstuvwxyz";
+        $uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $numbers = "0123456789";
+        $symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+        $allCharachters = $lowercase . $uppercase . $numbers . $symbols;
+
+        $sumCharacters = strlen($allCharachters);
+
+        $newPassword = "";
+
+        for ($i=0; $i < $charLength; $i++) { 
+            $randChar = rand(0, $sumCharacters - 1);
+
+            $newPassword .= $allCharachters[$randChar];
+        };
+
+       return $newPassword;
+    };
+?>
--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 <?php
+
+    include_once __DIR__ . '/functions.php';
+
     $charLength = isset($_GET["charachterNumber"]) ? intval($_GET["charachterNumber"], 10) : '';
-    function generatePassword($charLength) {
-        $lowercase = "abcdefgijklmnopqrstuvwxyz";
-        $uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        $numbers = "0123456789";
-        $symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
 
-        $allCharachters = $lowercase . $uppercase . $numbers . $symbols;
-
-        $sumCharacters = strlen($allCharachters);
-
-        $newPassword = "";
-
-        for ($i=0; $i < $charLength; $i++) { 
-            $randChar = rand(0, $sumCharacters - 1);
-
-            $newPassword .= $allCharachters[$randChar];
-        };
-
-       return $newPassword;
-    };
     $newPassword = generatePassword($charLength);
 ?>
 
